2cd33280d725a0770a95cc5413898b241dadf1e6
Şakir Mehmetoğlu <sakir.mehmetoglu@gmail.com>
2023-02-07 19:31:39 +0300

    SEO için meta bilgiler güncellendi.

--- resources/views/_layout/head.blade.php
+++ resources/views/_layout/head.blade.php
@@ -1,45 +1,56 @@
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>@yield('title', 'depremyardim.com')</title>
-	<meta name="description" content="depremyardim.com">
-	<meta name="author" content="depremyardim.com">
+    <title>Deprem Yardım Bekleyen Çağrılar</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="depremyardim.com">
+    <meta name="description" content="Depremde göçük altında kalan insanların ve yakınlarının yardım çağrıları toplanıyor. Deprem illeri: Kahramanmaraş, Hatay, Gaziantep, Şanlıurfa, Malatya, Osmaniye, Adana, Kilis, Adıyaman, Diyarbakır">
 
-  <!-- Favicon -->
-  <link href="img/favicon.png" rel="shortcut icon">
+    <meta name="keywords" content="deprem,yardım,yardim,cagri,göçük,gocuk,kahramanmaraş,kahramanmaras,maraş,maras,hatay,diyarbakır,diyarbakir,adıyaman,adiyaman,kilis,osmaniye,malatya,şanlıurfa,sanlıurfa,urfa,gaziantep,antep,adana,ailem,bebek,çocuk,bina,yıkıldı,afad,kurtarma">
 
-  <!-- Google Fonts - Poppins -->
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">	
+    <link rel=canonical href=https://depremyardim.com/ />
 
-  <!-- Font Awesome CSS -->
-  <link href="{{asset('assets/vendor/fontawesome/css/all.min.css')}}" rel="stylesheet">
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org/",
+      "@type": "NGO"
+    }
+    </script>
 
-  <!-- Custom Font Icons -->
-  <link href="{{asset('assets/vendor/icomoon/css/iconfont.min.css')}}" rel="stylesheet">
+    <!-- Favicon -->
+    <link href="img/favicon.png" rel="shortcut icon">
 
-  <!-- Vendor CSS -->
-  <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{asset('assets/vendor/dmenu/css/menu.css')}}" rel="stylesheet">
-  <link href="{{asset('assets/vendor/hamburgers/css/hamburgers.min.css')}}" rel="stylesheet">
-  <link href="{{asset('assets/vendor/mmenu/css/mmenu.min.css')}}" rel="stylesheet">
-  <link href="{{asset('assets/vendor/filepond/css/filepond.css')}}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.css">
-  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
-  
-  <!-- Main CSS -->
-  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <!-- Google Fonts - Poppins -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
 
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-9X26M290K8"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+    <!-- Font Awesome CSS -->
+    <link href="{{asset('assets/vendor/fontawesome/css/all.min.css')}}" rel="stylesheet">
 
-    gtag('config', 'G-9X26M290K8');
-  </script>
+    <!-- Custom Font Icons -->
+    <link href="{{asset('assets/vendor/icomoon/css/iconfont.min.css')}}" rel="stylesheet">
 
-  @yield('css')
+    <!-- Vendor CSS -->
+    <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/dmenu/css/menu.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/hamburgers/css/hamburgers.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/mmenu/css/mmenu.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/vendor/filepond/css/filepond.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+
+    <!-- Main CSS -->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9X26M290K8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-9X26M290K8');
+    </script>
+
+    @yield('css')
 </head>
