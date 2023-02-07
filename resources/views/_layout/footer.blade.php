6c4d06553830952110c018eb9dbb6f9889bb1d1f
Şakir Mehmetoğlu <sakir.mehmetoglu@gmail.com>
2023-02-07 18:58:02 +0300

    Footer temizlendi.

--- resources/views/_layout/footer.blade.php
+++ resources/views/_layout/footer.blade.php
@@ -1,18 +1,3 @@
 <!-- Footer -->
-<footer class="main-footer">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8">
-        <ul id="subFooterLinks">
-          <li><a href="https://codecanyon.net/user/ultimatewebsolutions" target="_blank">With <i class="fa fa-heart pulse"></i> by UWS</a></li>
-          <li><a href="https://codecanyon.net/user/ultimatewebsolutions#contact" target="_blank">Need Help?</a></li>
-          <li><a href="https://codecanyon.net/cart/configure_before_adding/23794224" target="_blank">Purchase Sendy</a></li>
-        </ul>
-      </div>
-      <div class="col-md-4">
-        <div id="copy">© 2020 Sendy</div>
-      </div>
-    </div>
-  </div>
-</footer>
+<footer class="main-footer"></footer>
 <!-- Footer End -->
