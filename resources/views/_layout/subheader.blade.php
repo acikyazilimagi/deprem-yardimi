27aa7f3bd89d008e7b3ff8b43574fbc13e5df62f
Kazim Ozkurt <kazim_ozkurt2@hotmail.com>
2023-02-08 15:09:19 +0300

    Fixed header

--- resources/views/_layout/subheader.blade.php
+++ resources/views/_layout/subheader.blade.php
@@ -3,13 +3,33 @@
     .header-menu > li {
         font-size: 20px;
     }
+    .nav-item-text{
+        font-size: 20px;
+        white-space: nowrap;
+    }
+    .sub-header {
+        width: 100%;
+        flex-wrap: nowrap;
+        overflow-x: auto;
+    }
+    .sub-header::-webkit-scrollbar {
+        display: none;
+    }
+    @media screen  and (max-width: 768px) {
+        .nav-item-text{
+            font-size: 14px;
+        }
+        .header-menu > * + * {
+            margin-left: 10px;
+        }
+    }
 </style>
 
 <div class="sub-header">
   <div class="container">
       <ul class="d-flex justify-content-between p-0 m-0 header-menu">
-          <li><a href="{{route('dashboard')}}" class="text-white">Deprem Yardım Bildirimi</a></li>
-          <li><a href="{{route('filter.index')}}" class="text-white">Detaylı Filtre</a></li>
+          <li><a href="{{route('dashboard')}}" class="nav-item-text text-white">Deprem Yardım Bildirimi</a></li>
+          <li><a href="{{route('filter.index')}}" class="nav-item-text text-white">Detaylı Filtre</a></li>
       </ul>
   </div>
 </div>
