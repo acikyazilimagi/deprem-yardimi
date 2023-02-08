c0b19be76f5b5886964179f1a5d9a747f3f3217a
Şakir Mehmetoğlu <sakir.mehmetoglu@gmail.com>
2023-02-08 15:57:24 +0300

    Gereksiz rota silindi.

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ Route::prefix('/')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/liste', [DashboardController::class, 'list'])->name('dashboard.list');
     Route::get('/datatable', [DashboardController::class, 'datatable'])->name('dashboard.datatable');
-    Route::post("/store", [DashboardController::class, "store"])->name("dashboard.store")->middleware(['auth-token-without-user']);
 });
 Route::post('get_district', [DashboardController::class, 'get_district'])->name('get_district');
 Route::post('get_street', [DashboardController::class, 'get_street'])->name('get_street');
