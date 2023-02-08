103114f19ad36a1065ba5a40e48774c0c20dec9b
orion <oriononair@gmail.com>
2023-02-08 08:32:29 +0300

    feat(lang): add datatables Turkish translation
    - Adds some basic Turkish translations using the language property

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -95,6 +95,36 @@
         search: {
             return: true,
         },
+        language: {
+            "sDecimal":        ",",
+            "sEmptyTable":     "Tabloda herhangi bir veri mevcut değil",
+            "sInfo":           "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
+            "sInfoEmpty":      "Kayıt yok",
+            "sInfoFiltered":   "(_MAX_ kayıt içerisinden bulunan)",
+            "sInfoThousands":  ".",
+            "sLengthMenu":     "Sayfada _MENU_ kayıt göster",
+            "sLoadingRecords": "Yükleniyor...",
+            "sProcessing":     "İşleniyor...",
+            "sSearch":         "Ara:",
+            "sZeroRecords":    "Eşleşen kayıt bulunamadı",
+            "oPaginate": {
+                "sFirst":    "İlk",
+                "sLast":     "Son",
+                "sNext":     "Sonraki",
+                "sPrevious": "Önceki"
+            },
+            "oAria": {
+                "sSortAscending":  ": artan sütun sıralamasını aktifleştir",
+                "sSortDescending": ": azalan sütun sıralamasını aktifleştir"
+            },
+            "select": {
+                "rows": {
+                    "_": "%d kayıt seçildi",
+                    "0": "",
+                    "1": "1 kayıt seçildi"
+                }
+            }
+        },
     });
 
     $('.form_submit').on('click', function (e){
