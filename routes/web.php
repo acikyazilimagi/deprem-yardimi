d929b8b0a1aa0742bd7994adba5d479fcb5d747e
Şakir Mehmetoğlu <sakir.mehmetoglu@gmail.com>
2023-02-08 17:54:18 +0300

    Anasayfadaki datagrid başka sayfaya taşındı.

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::prefix('/')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/liste', [DashboardController::class, 'list'])->name('dashboard.list');
     Route::get('/datatable', [DashboardController::class, 'datatable'])->name('dashboard.datatable');
+    Route::get('/yardim-isteyenler', [DashboardController::class, 'fast_search'])->name('fast_search');
 });
 Route::post('get_district', [DashboardController::class, 'get_district'])->name('get_district');
 Route::post('get_street', [DashboardController::class, 'get_street'])->name('get_street');
