3889a8d827e6c00a05ab2532f9a539a4c4ec50f5
Mehmet Coşkun <mehmet@coskun.dev>
2023-02-09 23:53:07 +0300

    [UPDATE]: Route's reformated

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', DashboardController::class)->name('dashboard.index');
 
-Route::group(['as' => 'help.'], function (){
+Route::group(['as' => 'help.'], function () {
     Route::get('/yardim-isteyenler', [\App\Http\Controllers\Web\HelpController::class, 'index'])->name('index');
     Route::get('/help/datatable', [\App\Http\Controllers\Web\HelpController::class, 'datatable'])->name('datatable');
 });
 
-Route::group(['prefix' => 'security', 'as' => 'security.'], function (){
+Route::group(['prefix' => 'security', 'as' => 'security.'], function () {
     Route::get('/get-token', \App\Http\Controllers\SecurityController::class)->name('get-token')->middleware(['auth-token-without-user']);
 });
 
-Route::group(['prefix' => '', 'as' => 'filter.'], function (){
+Route::group(['prefix' => '', 'as' => 'filter.'], function () {
     Route::get('/filtre', [\App\Http\Controllers\Web\FilterController::class, 'index'])->name('index');
     Route::post('/filter', [\App\Http\Controllers\Web\FilterController::class, 'filter'])->name('filter');
 });
