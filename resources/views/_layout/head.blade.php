bdd3abaaf7eec77be6f150557f9ae682b1aba512
Şakir Mehmetoğlu <sakir.mehmetoglu@gmail.com>
2023-02-09 03:31:06 +0300

    Meta bilgilerine title eklendi.

--- resources/views/_layout/head.blade.php
+++ resources/views/_layout/head.blade.php
@@ -12,12 +12,14 @@
 
     <link rel="icon" href="{{ asset('/assets/img/favicon/favicon.ico') }}">
 
+    <meta property="og:site_name" content="depremyardim.com" />
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{ route('dashboard.index') }}" >
     <meta property="og:title" content="Deprem Yardım Çağrısı ve Arama Kurtarma">
     <meta property="og:description" content="Depremde göçük altında kalan insanların ve yakınlarının yardım çağrıları toplanıyor.">
     <meta property="og:image" content="{{ asset('/assets/img/image.png') }}" >
 
+    <meta name="twitter:site_name" content="depremyardim.com" />
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="Deprem Yardım Çağrısı ve Arama Kurtarma">
     <meta name="twitter:description" content="Depremde göçük altında kalan insanların ve yakınlarının yardım çağrıları toplanıyor.">
